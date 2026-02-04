e4c8e430a9c629d58c89dbbb48a8ea61a20c1d58
nandana <nandanapnandu2852004@gmail.com>
2026-02-04 12:29:16 +0530

    Fixed folder structure, updated core URLs and integrated Day 3 to Day 8 tasks

--- python-internship-day7/core/urls.py
+++ python-internship-day7/core/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+# import view from app folder
+from apps.views import (
+    home, JobListAPI, JobCreateAPI,
+    ApplicationListAPI, ApplicationCreateAPI
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Day 3 Task URL (Root URL)
+    path('', home, name='home'),
+
+    # Day 5/7 API URLs
+    path('api/jobs/', JobListAPI.as_view(), name='job-list'),
+    path('api/jobs/create/', JobCreateAPI.as_view(), name='job-create'),
+    path('api/applications/', ApplicationListAPI.as_view(), name='application-list'),
+    path('api/applications/create/',
+         ApplicationCreateAPI.as_view(), name='application-create'),
 ]
