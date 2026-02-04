587fe256614a6a0b62befadf5eb4cb41fdfd6c2c
nandana <nandanapnandu2852004@gmail.com>
2026-02-04 11:50:56 +0530

    Integrated custom User model, organized app structure and fixed migration issues

--- python-internship-day7/core/settings.py
+++ python-internship-day7/core/settings.py
@@ -47,6 +47,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps',
+    'core',
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'apps.User'
